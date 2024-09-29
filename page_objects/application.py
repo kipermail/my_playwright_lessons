6c07bcf889410e88d194425e4a8adb3e6b2500ed
kipermail <kiper_mail@i.ua>
2024-09-30 01:35:56 +0300

    Lesson_10_2 TCM PageObject Test Cases

--- page_objects/application.py
+++ page_objects/application.py
@@ -1,37 +1,39 @@
 from playwright.sync_api import Playwright
+from page_objects.test_cases import Test_cases
 
 
 class App():
-    def __init__(self, playwright: Playwright):
-        self.browser = playwright.chromium.launch(headless=False, devtools=True)
+    def __init__(self, playwright: Playwright, base_url: str, headless=False):
+        self.browser = playwright.chromium.launch(headless=headless)
+        #self.browser = playwright.chromium.launch(headless=headless, devtools=devtools)
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
-        self.page.goto("http://127.0.0.1:8000/login/?next=/")
+        self.base_url = base_url
+        self.test_cases = Test_cases(self.page)
+       
+
+    def goto(self, endpoint: str, use_base_url=True):   
+        if use_base_url: 
+            self.page.goto(self.base_url + endpoint)
+        else: 
+            self.page.goto(endpoint)
+
+    def navigate_to(self, menu: str):
+         self.page.click(f"css=header >> text='{menu}'")
+
 
     
-    def login(self):
-        self.page.get_by_label("Username:").fill("alice")
-        self.page.get_by_label("Password:").fill("Qamania123")
+    def login(self, login: str, password: str):
+        self.page.get_by_label("Username:").fill(login)
+        self.page.get_by_label("Password:").fill(password)
         self.page.get_by_role("button", name="Login").click()
 
 
-    def create_test(self):
-        self.page.click("text='Create new test'")
-        self.page.fill("input[name='name']", "hello")
-        self.page.fill("textarea[name='description']", "world")
+    def create_test(self, test_name: str, test_descroption: str):
+        self.page.fill("input[name='name']", test_name)
+        self.page.fill("textarea[name='description']", test_descroption)
         self.page.click("input[type='submit']")
 
-
-    def open_tests(self):
-        self.page.click("text='Test Cases'")
-
-
-    def check_test_created(self):
-        self.page.query_selector("//td[text() ='hello']") is not None
-
-
-    def delete_test(self):    
-        self.page.click("//tr[13]/td[9]/button[normalize-space(.)='Delete']")
 
 
     def close(self):
--- tests/test_testcases.py
+++ tests/test_testcases.py
@@ -1,7 +1,25 @@
-def test_new_testcases(desktop_app):
-    desktop_app.login()
-    desktop_app.create_test()
-    desktop_app.open_tests()
-    desktop_app.check_test_created()
-    desktop_app.delete_test()
+def test_new_testcase(desktop_app_auth):
+    test_name = "hello"
+    desktop_app_auth.navigate_to("Create new test")
+    desktop_app_auth.create_test(test_name, "world")
+    desktop_app_auth.navigate_to("Test Cases")
+    assert desktop_app_auth.test_cases.check_test_exists(test_name)
+    desktop_app_auth.test_cases.delete_test_by_name(test_name)
     #desktop_app.close()
+
+def test_new_testcase_no_description(desktop_app_auth):
+    test_name = "hello"
+    desktop_app_auth.navigate_to("Create new test")
+    desktop_app_auth.create_test(test_name, "")
+    desktop_app_auth.navigate_to("Test Cases")
+    assert desktop_app_auth.test_cases.check_test_exists(test_name)
+    desktop_app_auth.test_cases.delete_test_by_name(test_name)
+
+
+def test_new_testcase_digits_name(desktop_app_auth):
+    test_name = "123"
+    desktop_app_auth.navigate_to("Create new test")
+    desktop_app_auth.create_test(test_name, "world")
+    desktop_app_auth.navigate_to("Test Cases")
+    assert desktop_app_auth.test_cases.check_test_exists(test_name)
+    desktop_app_auth.test_cases.delete_test_by_name(test_name)
