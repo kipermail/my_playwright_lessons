3d21180f4c295d3eb305a22501fc2177e897fb72
kipermail <kiper_mail@i.ua>
2024-09-29 20:26:39 +0300

    Lesson_1 TCM page changed selectors

--- lesson_1_test_TC.py
+++ lesson_1_test_TC.py
@@ -6,32 +6,28 @@ def run(playwright: Playwright) -> None:
     context = browser.new_context()
     page = context.new_page()
     page.goto("http://127.0.0.1:8000/login/?next=/")
-    page.get_by_label("Username:").click()
     page.get_by_label("Username:").fill("alice")
-    page.get_by_label("Password:").click()
     page.get_by_label("Password:").fill("Qamania123")
     page.get_by_role("button", name="Login").click()
     page.get_by_role("link", name="Create new test").click()
-    page.locator("#id_name").click()
-    page.locator("#id_name").fill("hello")
-    page.get_by_label("Test description").click()
-    page.get_by_label("Test description").fill("world")
-    page.get_by_role("button", name="Create").click()
-    page.get_by_role("link", name="Test Cases").click()
+    
+    page.click("text='Create new test'")
+    page.fill("input[name='name']", "hello")
+    page.fill("textarea[name='description']", "world")
+    page.click("input[type='submit']")
+
+    page.click("text='Test Cases'")
 
     assert page.query_selector("//td[text() ='hello']") is not None
 
-    page.get_by_role("row", name="hello world alice Norun None PASS FAIL Details Delete").get_by_role("button").nth(3).click()
+    page.click("//tr[13]/td[9]/button[normalize-space(.)='Delete']")
 
     page.close()
-
-    # ---------------------
-    
-
-
     context.close()
     browser.close()
 
-# def test_createTC():
-with sync_playwright() as playwright:
-    run(playwright)
+
+
+def test_create_TC():
+    with sync_playwright() as playwright:
+        run(playwright)
