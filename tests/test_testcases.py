9a9d172693a61fc57a946e226bbc7407d1bcdd22
kipermail <kiper_mail@i.ua>
2024-09-30 21:09:19 +0300

    test ver 2

--- tests/test_testcases.py
+++ tests/test_testcases.py
@@ -1,25 +1,17 @@
-def test_new_testcase(desktop_app_auth):
-    test_name = "hello"
+from pytest import mark
+
+test_data = [
+    ("hello", "world"),
+    ("hello", ""),
+    ("123", "world123"),
+]
+
+@mark.parametrize("test_name, description", test_data, ids=["general test", "empty description", "number in name"])
+def test_new_testcase(desktop_app_auth, test_name, description):
     desktop_app_auth.navigate_to("Create new test")
-    desktop_app_auth.create_test(test_name, "world")
+    desktop_app_auth.create_test(test_name, description)
     desktop_app_auth.navigate_to("Test Cases")
     assert desktop_app_auth.test_cases.check_test_exists(test_name)
     desktop_app_auth.test_cases.delete_test_by_name(test_name)
     #desktop_app.close()
 
-def test_new_testcase_no_description(desktop_app_auth):
-    test_name = "hello"
-    desktop_app_auth.navigate_to("Create new test")
-    desktop_app_auth.create_test(test_name, "")
-    desktop_app_auth.navigate_to("Test Cases")
-    assert desktop_app_auth.test_cases.check_test_exists(test_name)
-    desktop_app_auth.test_cases.delete_test_by_name(test_name)
-
-
-def test_new_testcase_digits_name(desktop_app_auth):
-    test_name = "123"
-    desktop_app_auth.navigate_to("Create new test")
-    desktop_app_auth.create_test(test_name, "world")
-    desktop_app_auth.navigate_to("Test Cases")
-    assert desktop_app_auth.test_cases.check_test_exists(test_name)
-    desktop_app_auth.test_cases.delete_test_by_name(test_name)
